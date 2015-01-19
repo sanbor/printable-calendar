abc0c029768ff08873db94796222c9ef0306b0c1
Santiago Borrazas <sanbor@gmail.com>
2015-01-19 07:31:23 -0300

    Tons of improvements and bugfixes

--- calendar.js
+++ calendar.js
@@ -1,149 +1,293 @@
-function toInches(number) {
-  return Number(number / 25.4).toPrecision(2);
-}
+// add delay to the keyup event, borrowed from http://stackoverflow.com/questions/1909441/jquery-keyup-delay
+var delay = (function() {
+  var timer = 0;
+  return function(callback, ms) {
+    clearTimeout(timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
 
+var holidays = ['01-01', '02-16', '02-17', '03-23', '03-24', '04-02', '04-03', '05-01', '05-25', '06-20', '07-09', '08-17', '10-12', '11-23', '12-07', '12-08', '12-25'];
+
+// optimization: maybe it would be a better idea to create image nodes and store a reference to the node
+var pictures = ['photos/1/1.jpg', 'photos/1/2.jpg', 'photos/1/3.jpg', 'photos/1/4.jpg', 'photos/1/5.jpg', 'photos/1/6.jpg', 'photos/1/7.jpg', 'photos/1/8.jpg', 'photos/1/9.jpg', 'photos/1/10.jpg', 'photos/1/11.jpg', 'photos/1/12.jpg'];
+
+// sizes in mm
+var PAPER_SIZES = {
+  'a4': {
+    width: 210,
+    height: 297,
+    marginTop: 17,
+    marginBottom: 17,
+    marginLeft: 17,
+    marginRight: 17
+  },
+  'a3': {
+    width: 297,
+    height: 420,
+    marginTop: 17,
+    marginBottom: 17,
+    marginLeft: 17,
+    marginRight: 17
+  },
+  'letter': {
+    width: 215.9,
+    height: 279.4,
+    marginTop: 17,
+    marginBottom: 17,
+    marginLeft: 17,
+    marginRight: 17
+  }
+};
 
 $(function() {
-  var date = moment().date(1).month(0);
-  var year = moment(date).year();
-  // sizes in mm
-  var pageSizes = {
-    'a4': {
-      width: 210,
-      height: 297,
-      marginTop: 17,
-      marginBottom: 25.4,
-      marginLeft: 17,
-      marginRight: 17
-    },
-    'a3': {
-      width: 297,
-      height: 420,
-      marginTop: 17,
-      marginBottom: 25.4,
-      marginLeft: 17,
-      marginRight: 17
-    },
-    'letter': {
-      width: 215.9,
-      height: 279.4,
-      marginTop: 17,
-      marginBottom: 25.4,
-      marginLeft: 17,
-      marginRight: 17
-    }
-  };
+  populateYearsDropdown();
+  addFormEvents();
+  addCalendarEvents();
 
-  $('.fromPeriod').val(moment(date).year(++year).format('YYYY-MM-DD'));
-  $('.toPeriod').val(moment(date).year(++year).format('YYYY-MM-DD'));
+  updatePaperFormControls();
 
-  $('.widthUnit').change(function(event) {
-    $('.units').text($('.widthUnit').val());
-    $('.pageSize').change();
-  });
-
-  $('.widthUnit').change();
-
-  $('.pageSize').change(function(event) {
-    if ($('.pageSize').val() === 'custom') {
-      $('.customPageSize').show();
-    } else {
-      $('.customPageSize').hide();
-      var pageSize = pageSizes[$('.pageSize').val()];
-      var isMm = $('.widthUnit').val() === 'mm';
-      $('.pageMarginTop').val(isMm ? pageSize.marginTop : toInches(pageSize.marginTop));
-      $('.pageMarginBottom').val(isMm ? pageSize.marginBottom : toInches(pageSize.marginBottom));
-      $('.pageMarginLeft').val(isMm ? pageSize.marginLeft : toInches(pageSize.marginLeft));
-      $('.pageMarginRight').val(isMm ? pageSize.marginRight : toInches(pageSize.marginRight));
-    }
-  });
-
-  $('.printCalendar').click(function(event) {
-    event.preventDefault();
-    var pictureSeparate = $('.pictureSeparate').prop('checked');
-    var pageWidth;
-    var pageHeight;
-    var unit = $('.widthUnit').val();
-    var pageMarginTop = $('.pageMarginTop').val();
-    var pageMarginBottom = $('.pageMarginBottom').val();
-    var pageMarginLeft = $('.pageMarginLeft').val();
-    var pageMarginRight = $('.pageMarginRight').val();
-
-    if ($('.pageSize').val() === 'custom') {
-      // when the picture is separate, we want to print in landscape
-      // so the width it's the height and the height the width
-      if (pictureSeparate) {
-        pageWidth = $('.pageWidth').val() - pageMarginLeft - pageMarginRight;
-        pageHeight = ($('.pageHeight').val() * 2) - pageMarginTop - pageMarginBottom;
-      } else {
-        pageWidth = $('.pageHeight').val() - pageMarginLeft - pageMarginRight;
-        pageHeight = $('.pageWidth').val() - pageMarginTop - pageMarginBottom;
-      }
-
-      less.modifyVars({
-        '@pageSizeUnit': unit,
-        '@pageWidth': pageWidth + unit,
-        '@pageHeight': pageHeight + unit,
-        '@pageSize': '@pageWidt @pageHeight',
-        '@pageOrientation': pictureSeparate ? 'landscape' : 'portrait',
-      });
-    } else {
-      var selectedSize = $('.pageSize').val();
-      var pageSize = pageSizes[selectedSize];
-
-      // when the picture is separate, we want to print in landscape
-      // so the width it's the height and the height the width
-      if (pictureSeparate) {
-        // convert mm to in if necessary
-        pageWidth = unit === 'mm' ? pageSize.height : toInches(pageSize.height);
-        pageHeight = unit === 'mm' ? pageSize.width : toInches(pageSize.width);
-        pageHeight *= 2;
-        pageHeight = pageHeight - pageMarginTop - pageMarginBottom;
-      } else {
-        // convert mm to in if necessary
-        pageWidth = unit === 'mm' ? pageSize.width : pageSize.width / 25.4;
-        pageHeight = unit === 'mm' ? pageSize.height : pageSize.height / 25.4;
-      }
-      pageWidth = pageWidth - pageMarginLeft - pageMarginRight;
-      pageHeight = pageHeight - pageMarginTop - pageMarginBottom - 1;
-
-      less.modifyVars({
-        '@pageSizeUnit': unit,
-        '@pageWidth': pageWidth + unit,
-        '@pageHeight': pageHeight + unit,
-        '@pageSize': selectedSize,
-        '@pageOrientation': pictureSeparate ? 'landscape' : 'portrait',
-      });
-    }
-
-    $('body').toggleClass('pictureSeparate', pictureSeparate);
-    $('body').toggleClass('pageBorder', $('.showPageBorder').prop('checked'));
-    $('body').toggleClass('noPictures', $('.noPictures').prop('checked'));
-
-    var from = $('.fromPeriod').val();
-    var to = $('.toPeriod').val();
-    generateCalendar(from, to);
-
-    window.print();
-  });
+  // Shows the preview
+  updateCalendar();
 });
 
-function generateCalendar(from, to) {
+function populateYearsDropdown() {
+  var year = moment().year();
+
+  for (var i = year; i < (year + 10); i++) {
+    $('.year').append('<option>' + i + '</option');
+  };
+
+  // checks if it's time to print next year calendar
+  if (moment().month() > 10) {
+    $('.year option:nth-child(2)').prop('selected', true);
+  }
+}
+
+function isNumber(n) {
+  return typeof n == 'number' && !isNaN(n) && isFinite(n);
+}
+
+function areSizesValid() {
+  var isValid = true;
+
+  $('input').each(function() {
+    var value = Number($(this).val());
+    if (!isNumber(value)) {
+      isValid = false;
+    }
+  });
+
+  return isValid;
+};
+
+function updateCalendar() {
+  if (!areSizesValid()) {
+    return;
+  }
+
+  updatePaperSettings();
+  generateCalendar($('.year').val());
+}
+
+function addFormEvents() {
+  $('.printCalendar').click(printCalendar);
+  $('.widthUnit').change(updatePaperFormControls);
+  $('.paperSize').change(updatePaperFormControls);
+  $('select').change(updateCalendar);
+  $('.tab-content input')
+    .on('keyup', function() {
+      delay(function() {
+        updatePaperSettings();
+        generateCalendar($('.year').val());
+      }, 400);
+    });
+}
+
+var dragSrcEl_ = null;
+
+function addCalendarEvents() {
+  $('.printableCalendar').on('click', '.Calendar-table td', function(event) {
+    var holidayDate = $(event.currentTarget).data('date');
+    var existingHoliday = holidays.indexOf(holidayDate);
+
+    if (existingHoliday < 0) {
+      if (!$(event.currentTarget).hasClass('prev-month')) {
+        holidays.push(holidayDate);
+        $('.Calendar-table td[data-date=' + holidayDate + ']').addClass('holiday');
+      }
+    } else {
+      holidays.splice(existingHoliday, 1);
+      $('.Calendar-table td[data-date=' + holidayDate + ']').removeClass('holiday');
+    }
+  });
+
+  $('.printableCalendar').on('dragstart', '.imgContainer', function(event) {
+    event.originalEvent.dataTransfer.effectAllowed = 'move';
+    event.originalEvent.dataTransfer.setData('text/html', this.innerHTML);
+    dragSrcEl_ = this;
+    $(event.currentTarget).css({
+      'border': 'dashed 2px blue'
+    });
+  });
+
+  $('.printableCalendar').on('dragover', '.imgContainer', function(event) {
+    event.preventDefault();
+    $(event.currentTarget).css({
+      'border': 'dashed 2px blue'
+    });
+  });
+
+  $('.printableCalendar').on('dragend', '.imgContainer', function(event) {
+    event.preventDefault();
+    $(event.currentTarget).css({
+      'border': 'none'
+    });
+  });
+
+  $('.printableCalendar').on('drop', '.imgContainer', function(event) {
+    event.preventDefault();
+    console.log(event.originalEvent.dataTransfer.files.length);
+    if (event.originalEvent.dataTransfer.files.length === 0) {
+      // Don't do anything if we're dropping on the same column we're dragging.
+      if (dragSrcEl_ != this) {
+        dragSrcEl_.innerHTML = this.innerHTML;
+        this.innerHTML = event.originalEvent.dataTransfer.getData('text/html');
+      }
+    } else {
+      readfiles(event.originalEvent.dataTransfer.files, $(event.currentTarget));
+    }
+    $('.imgContainer').css({
+      'border': 'none'
+    });
+  });
+}
+
+var tests = {
+  filereader: typeof FileReader != 'undefined',
+  dnd: 'draggable' in document.createElement('span'),
+  formdata: !!window.FormData,
+};
+
+var acceptedTypes = {
+  'image/png': true,
+  'image/jpeg': true,
+  'image/gif': true
+};
+
+function showfile(file, $imageContainer, containerIndex) {
+  if (tests.filereader === true && acceptedTypes[file.type] === true) {
+    var reader = new FileReader();
+    reader.onload = function(event) {
+      var image = new Image();
+      image.src = event.target.result;
+      $imageContainer.empty();
+      $imageContainer.append(image);
+      pictures[containerIndex] = image.src;
+    };
+
+    reader.readAsDataURL(file);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function readfiles(files, $imageContainer) {
+  var containerIndex = $('.imgContainer').index($imageContainer);
+
+  for (var i = 0; i < files.length; i++) {
+    // if (tests.formdata) formData.append('file', files[i]);
+    var shown = showfile(files[i], $('.imgContainer:eq(' + containerIndex + ')'), containerIndex);
+    if (shown) {
+      containerIndex++;
+    }
+  }
+}
+
+function toMm(number, unit) {
+  var n = Number(number);
+
+  return unit == 'mm' ? n : (n * 25.4).toPrecision(2);
+}
+
+function toInches(number) {
+  var n = Number(number);
+
+  return (n / 25.4).toPrecision(2);
+}
+
+function updatePaperSettings() {
+  var unit = $('.widthUnit').val();
+  var selectedSize;
+  var paperWidth;
+  var paperHeight;
+  var paperMarginTop = $('.paperMarginTop').val();
+  var paperMarginBottom = $('.paperMarginBottom').val();
+  var paperMarginLeft = $('.paperMarginLeft').val();
+  var paperMarginRight = $('.paperMarginRight').val();
+
+  if ($('.paperSize').val() === 'custom') {
+    selectedSize = '';
+    paperWidth = toMm($('.paperWidth').val(), unit);
+    paperHeight = toMm($('.paperHeight').val(), unit);
+
+  } else {
+    selectedSize = $('.paperSize').val();
+    selectedPaper = PAPER_SIZES[selectedSize];
+    paperWidth = selectedPaper.width;
+    paperHeight = selectedPaper.height;
+  }
+
+  less.modifyVars({
+    '@paperSizeUnit': unit,
+    '@paperWidth': paperWidth + 'mm',
+    '@paperHeight': paperHeight + 'mm',
+    '@paperMarginTop': toMm(paperMarginTop, unit) + 'mm',
+    '@paperMarginBottom': toMm(paperMarginBottom, unit) + 'mm',
+    '@paperMarginLeft': toMm(paperMarginLeft, unit) + 'mm',
+    '@paperMarginRight': toMm(paperMarginRight, unit) + 'mm',
+    '@paperSize': selectedSize,
+    '@paperOrientation': 'portrait',
+  });
+}
+
+function printCalendar(event) {
+  updatePaperSettings();
+  window.print();
+  event.preventDefault();
+};
+
+function updatePaperFormControls() {
+  $('.units').text($('.widthUnit').val());
+
+  if ($('.paperSize').val() === 'custom') {
+    $('.custompaperSize').show();
+  } else {
+    $('.custompaperSize').hide();
+    var paperSize = PAPER_SIZES[$('.paperSize').val()];
+    var isMm = $('.widthUnit').val() === 'mm';
+    $('.paperMarginTop').val(isMm ? paperSize.marginTop : toInches(paperSize.marginTop));
+    $('.paperMarginBottom').val(isMm ? paperSize.marginBottom : toInches(paperSize.marginBottom));
+    $('.paperMarginLeft').val(isMm ? paperSize.marginLeft : toInches(paperSize.marginLeft));
+    $('.paperMarginRight').val(isMm ? paperSize.marginRight : toInches(paperSize.marginRight));
+  }
+};
+
+function generateCalendar(year) {
   // clean previous calendar
   $('.printableCalendar').empty();
 
   // Spanish days
   moment.locale('es');
   var startOnModay = true;
-
-  var from = moment(from);
-  var to = moment(to);
+  var from = moment(year + '-01-01');
+  var to = moment((parseInt(year, 10) + 1) + '-01-01');
 
   var months = moment.months();
   var currentMonth = 0;
   var week = 1;
   var $table;
-  var holidays = ['01-01', '02-16', '02-17', '03-23', '03-24', '04-02', '04-03', '05-01', '05-25', '06-20', '07-09', '08-17', '10-12', '11-23', '12-07', '12-08', '12-25'];
 
   // First week of the year (starts in Dec 28 2014 instead Jan 1 2015)
   var date = moment(from)
@@ -156,15 +300,15 @@ function generateCalendar(from, to) {
     date.day(0);
   }
 
-  var createPageForMonthNumber = function(month) {
-    var $container = $('<div class="page"></div>').appendTo('.printableCalendar');
+  var createpaperForMonthNumber = function(month) {
+    var $container = $('<div class="paper"></div>').appendTo('.printableCalendar');
 
     insertPicture(month, $container);
     insertCalendar(month, $container);
   }
 
   var insertPicture = function(month, $container) {
-    $('<div class="imgContainer"><img src="photos/1/' + (month + 1) + '.jpg" /></div>').appendTo($container);
+    $('<div class="imgContainer" draggable="true"><img src="' + pictures[month] + '" /></div>').appendTo($container);
   };
 
   var insertCalendar = function(month, $container) {
@@ -194,28 +338,29 @@ function generateCalendar(from, to) {
   };
 
   // January
-  createPageForMonthNumber(0);
+  createpaperForMonthNumber(0);
 
   while (date.isBefore(to)) {
     var $tr = $('<tr/>').appendTo($table);
 
     for (var i = 0; i < 7; i++) {
       var day = date.format('DD');
+      var dataDate = 'data-date="' + date.format('MM-DD') + '"';
+
+      var dayElement = $('<td ' + dataDate + '>' + day + '</td>');
 
       if (holidays.indexOf(date.format('MM-DD')) >= 0) {
-        day = '<span class="holiday">' + day + '</span>';
+        dayElement.addClass('holiday');
       }
-
-      var dayElement = '<td>' + day + '</td>';
       // Checks if the current day is from last month OR last year
       // BUG: doesn't work in January with calendars longer than a year
       if (date.month() < currentMonth || date.year() < from.year()) {
-        dayElement = '<td class="prev-month">' + day + '</td>';
+        dayElement.addClass('prev-month');
       }
       // Checks if the current day is from the next month and this year
       // OR the day is from the next year
       if ((date.month() > currentMonth && date.year() >= from.year()) || date.year() >= to.year()) {
-        dayElement = '<td class="next-month">' + day + '</td>';
+        dayElement.addClass('prev-next-month');
       }
 
       $tr.append(dayElement);
@@ -230,7 +375,7 @@ function generateCalendar(from, to) {
         currentMonth = 0;
       }
 
-      createPageForMonthNumber(currentMonth);
+      createpaperForMonthNumber(currentMonth);
 
       // if Monday is not 1st, we are going to show the last days of the
       // last month
